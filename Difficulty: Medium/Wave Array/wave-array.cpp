3773885cd1c241382e4ed07b6a0eefaaba4e7db0
PrathamGhiya <ghiya.pratham@gmail.com>
2026-08-09 18:15:04 +0530

    Update wave-array.cpp

--- Difficulty: Medium/Wave Array/wave-array.cpp
+++ Difficulty: Medium/Wave Array/wave-array.cpp
@@ -3,7 +3,7 @@ class Solution {
     void sortInWave(vector<int>& arr) {
         int i=0;
         while(i<arr.size()-1){
-            if(i%2==0 && arr[i]<arr[i+1]) swap(arr[i],arr[i+1]);
+            swap(arr[i],arr[i+1]);
             i+=2;
         }
     }
